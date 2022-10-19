35da0cd5a34d93889df2feece395279344b200f3
Keila Lopez <keilalofra@gmail.com>
2022-10-19 14:45:39 -0500

    Deliverables 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetchFunction()
+    
 })
+
+// tab for yes, backarrow for no
+// DELIVERABLE 1
+function fetchFunction () {
+    fetch('http://localhost:3000/dogs')
+    .then (response => response.json())
+    .then (dogListArray => dogListArray.forEach(dogObj => {
+        renderDog(dogObj)
+    }))
+}
+
+
+function renderDog (dogObj) {
+    const registeredDogsTable = document.querySelector('table')
+
+    const dogRow = document.createElement('tr')
+
+    const dogCell = document.createElement('td')
+    const breedCell = document.createElement('td')
+    const sexCell = document.createElement('td')
+    const editDogCell = document.createElement('td')
+    
+    dogCell.textContent = dogObj.name
+    breedCell.textContent = dogObj.breed
+    sexCell.textContent = dogObj.sex
+    editDogCell.innerHTML = `<button>Edit Dog</button>`
+    
+    registeredDogsTable.append(dogRow)
+    dogRow.append(dogCell, breedCell, sexCell, editDogCell) 
+
+}
