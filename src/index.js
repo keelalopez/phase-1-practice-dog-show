8163a08a44fe3fec177e44d26365f615c9deb4d3
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-19 16:43:58 -0400

    edit dog click event and submit event

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchFunction()
+
+    const form = document.querySelector("#dog-form");
+
     
-})
 
 // tab for yes, backarrow for no
 // DELIVERABLE 1
 function fetchFunction () {
     fetch('http://localhost:3000/dogs')
     .then (response => response.json())
-    .then (dogListArray => dogListArray.forEach(dogObj => {
+    .then (dogListArray => 
+        dogListArray.forEach(dogObj => {
         renderDog(dogObj)
+
+        form.addEventListener("submit", (e)=> {
+            e.preventDefault();
+    
+            const dogInputObject = {
+                name: form.name.value,
+                breed: form.breed.value,
+                sex: form.sex.value
+            }     
+            form.reset();
+            
+
+            
+        })
     }))
 }
+
+function patchEvent (dogInputObject){
+    fetch(`http://localhost:3000/dogs/${form.id.value}`, {
+    method: "PATCH", 
+    headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dogInputOBject)
+    })
+    .then (response => response.json())
+    .then (res => {
+        console.log(res);
+    });
+}
+
 
 
 function renderDog (dogObj) {
@@ -23,13 +55,38 @@ function renderDog (dogObj) {
     const breedCell = document.createElement('td')
     const sexCell = document.createElement('td')
     const editDogCell = document.createElement('td')
+    const editDogButton = document.createElement('button');
     
     dogCell.textContent = dogObj.name
     breedCell.textContent = dogObj.breed
     sexCell.textContent = dogObj.sex
-    editDogCell.innerHTML = `<button>Edit Dog</button>`
+    editDogButton.textContent = "Edit Dog";
+    editDogCell.append(editDogButton)
+
+     
     
     registeredDogsTable.append(dogRow)
     dogRow.append(dogCell, breedCell, sexCell, editDogCell) 
 
+    // let form = document.querySelector("#dog-form");
+
+    editDogButton.addEventListener("click", ()=> {
+        // dogCell.value
+        form.name.value = dogObj.name;
+        form.breed.value = dogObj.breed;
+        form.sex.value = dogObj.sex;
+
+        // editingDogInfo ();
+    })
+
 }
+
+
+
+//Make a dog editable. Clicking on the edit button next to a dog should populate the top 
+//form with that dog's current information.
+
+
+
+
+})
